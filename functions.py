805db9dd51df9a056aa7babe5a1c45d9a1ffd93f
gSchool Student <gSchool@Datas-Mac-mini-188.local>
2016-10-14 15:58:09 -0700

    have kmeans plot

--- functions.py
+++ functions.py
@@ -1,11 +1,22 @@
 import numpy as np
 import pandas as pd
 import re
+import os
+import gensim.utils
+import mpld3
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from sklearn.manifold import MDS
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cross_validation import cross_val_score
 from collections import defaultdict
+from sklearn.cluster import KMeans
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import AgglomerativeClustering
 
 def load_data(filename):
-    with open('new_train/{}'.format(filename)) as f:
+    with open('data/{}'.format(filename)) as f:
         d = defaultdict(list)
 
 
@@ -33,3 +44,8 @@ def lem(documents):
         no_tag_words = [w[:-3] for w in gensim.utils.lemmatize(doc)]
         lem_documents.append(' '.join(no_tag_words))
     return lem_documents
+
+def Tfidf(documents):
+    vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1,3)).fit(documents)
+    vectors = vectorizer.transform(documents)
+    return vectors, vectorizer
